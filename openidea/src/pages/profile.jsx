25918cd4de1712fd63f652b1d7580f427744f16e
NikSS <nik.safe@gmail.com>
2018-08-15 10:12:34 -0700

    fixed the merge conflict

--- openidea/src/pages/profile.jsx
+++ openidea/src/pages/profile.jsx
@@ -16,29 +16,17 @@ class ProfilePage extends Component  {
       await this.getDataFromAPI()
     }
   // loading messages from the server
-<<<<<<< HEAD
-   getDataFromAPI = async () => {
 
-  // fetch ideasJson,users and  categories
-      const ideasJson = await fetch('http://localhost:3000/ideas')
-      const usersJson = await fetch('http://localhost:3000/users')
-      // const categoriesJson = await fetch('http://localhost:3000/categories')
-=======
+
+
     getDataFromAPI = async () => {
       // fetch messagesJson
       const usersJson = await fetch('http://localhost:3000/users')
-      console.log(usersJson,"<<<after users fetch");
       const ideasJson = await fetch('http://localhost:3000/ideas')
       //add the categories api to the fron end
       // const  categoriesJson = await fetch('http://localhost:3000/categories')
->>>>>>> 1674722f793cb94fc7298fe5a8e2b99fdf10f5a5
-
       let users = await usersJson.json();
-<<<<<<< HEAD
-=======
       let ideas = await ideasJson.json();
->>>>>>> 1674722f793cb94fc7298fe5a8e2b99fdf10f5a5
-      // let categories = await categoriesJson.json();
 
       this.setState({
         ideas,
@@ -46,7 +34,7 @@ class ProfilePage extends Component  {
       })
     }
     //these functions are for callapsing the idea card with all the data in it
-    
+
   render(){
     return(
       <div>
